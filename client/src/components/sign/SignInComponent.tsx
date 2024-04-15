b1b83b75f46212fdb4bfb6d70fee464288e7697d
김민재 <minjae2246@ubcare.co.kr>
2024-04-16 08:42:16 +0900

    로그인 회원가입 프레임 완성

--- client/src/components/sign/SignInComponent.tsx
+++ client/src/components/sign/SignInComponent.tsx
@@ -3,11 +3,24 @@ import {
   FormControl,
   FormErrorMessage,
   FormLabel,
-  Input,
-  VStack
+  VStack,
 } from "@chakra-ui/react";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
+import { useState } from "react";
 
 export default function SignInComponent() {
+  const [show, setShow] = useState(false);
+  const [email, setEmail] = useState<string | null | undefined>();
+  const [password, setPassword] = useState<string | null | undefined>();
+
+  const handleClick = () => setShow(!show);
+
+  const handleInputChange = (
+    value: string,
+    setValue: React.Dispatch<React.SetStateAction<string | null | undefined>>
+  ) => {
+    setValue(value);
+  };
   return (
     <VStack
       as="form"
@@ -15,29 +28,43 @@ export default function SignInComponent() {
       m="auto"
       justify="center"
     >
-      <FormControl>
+      <FormControl isRequired>
         <FormLabel>이메일</FormLabel>
         <Input
-          name="id"
           placeholder="이메일을 입력해주세요"
           autoComplete="off"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            handleInputChange(e.target.value, setEmail)
+          }
         ></Input>
         <FormErrorMessage>유효하지 않은 아이디</FormErrorMessage>
       </FormControl>
-      <FormControl>
+      <FormControl isRequired>
         <FormLabel>비밀번호</FormLabel>
-        <Input
-          name="password"
-          placeholder="비밀번호를 입력해주세요"
-          autoComplete="off"
-        ></Input>
+        <InputGroup size="md">
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="비밀번호를 입력해주세요"
+            autoComplete="off"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              handleInputChange(e.target.value, setPassword)
+            }
+          ></Input>
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "숨기기" : "보기"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
       </FormControl>
-        <Button
-          colorScheme="blue"
-          width="100%"
-          style={{ marginTop: 15 }}
-          colorScheme="teal"
-        >로그인</Button>
+      <Button
+        colorScheme="blue"
+        width="100%"
+        style={{ marginTop: 15 }}
+        colorScheme="teal"
+      >
+        로그인
+      </Button>
     </VStack>
   );
 }
--- client/src/components/sign/SignUpComponent.tsx
+++ client/src/components/sign/SignUpComponent.tsx
@@ -2,100 +2,105 @@ import { Button } from "@chakra-ui/button";
 import { FormControl, FormLabel } from "@chakra-ui/form-control";
 import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
 import { VStack } from "@chakra-ui/layout";
-import { useToast } from "@chakra-ui/toast";
-// import axios from "axios";
 import { useState } from "react";
 
 export default function SignUpComponent() {
   const [show, setShow] = useState(false);
   const [nickname, setNickname] = useState<string | null | undefined>();
   const [email, setEmail] = useState<string | null | undefined>();
-  const [confirmPassword, setConfirmPassword] = useState<string | null | undefined>();
   const [password, setPassword] = useState<string | null | undefined>();
+  const [confirmPassword, setConfirmPassword] = useState<
+    string | null | undefined
+  >();
   const [pic, setPic] = useState<FileList | null>();
-  const [picLoading, setPicLoading] = useState(false);
+
   const handleClick = () => setShow(!show);
-  const toast = useToast();
- 
-  const handleInputChange = (value: string, setValue: React.Dispatch<React.SetStateAction<string | null | undefined>>
+
+  const handleInputChange = (
+    value: string,
+    setValue: React.Dispatch<React.SetStateAction<string | null | undefined>>
   ) => {
     setValue(value);
   };
 
   const postDetails = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files: FileList | null = e.target.files
-    setPic(files)
-  }
+    const files: FileList | null = e.target.files;
+    setPic(files);
+  };
 
-
-
-  return <VStack spacing="5px">
-    <FormControl id="first-name" isRequired>
-      <FormLabel>닉네임</FormLabel>
-      <Input
-        placeholder="닉네임 입력"
-        onChange={(e: React.ChangeEvent<HTMLInputElement>)=>handleInputChange(e.target.value, setNickname)}
-      />
-    </FormControl>
-    <FormControl id="email" isRequired>
-      <FormLabel>이메일</FormLabel>
-      <Input
-        type="email"
-        placeholder="이메일 입력"
-        onChange={(e: React.ChangeEvent<HTMLInputElement>)=>handleInputChange(e.target.value, setEmail)}
-      />
-    </FormControl>
-    <FormControl id="password" isRequired>
-      <FormLabel>비밀번호</FormLabel>
-      <InputGroup size="md">
+  return (
+    <VStack spacing="5px">
+      <FormControl id="first-name" isRequired>
+        <FormLabel>닉네임</FormLabel>
         <Input
-          type={show ? "text" : "password"}
-          placeholder="비밀번호 입력"
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(e.target.value, setPassword)}
+          placeholder="닉네임 입력"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            handleInputChange(e.target.value, setNickname)
+          }
         />
-        <InputRightElement width="4.5rem">
-          <Button h="1.75rem" size="sm"
-          onClick={handleClick}
-          >
-            {show ? "감추기" : "보기"}
-          </Button>
-        </InputRightElement>
-      </InputGroup>
-    </FormControl>
-    <FormControl id="password" isRequired>
-      <FormLabel>비밀번호 확인</FormLabel>
-      <InputGroup size="md">
+      </FormControl>
+      <FormControl id="email" isRequired>
+        <FormLabel>이메일</FormLabel>
         <Input
-          type={show ? "text" : "password"}
-          placeholder="비밀번호 확인"
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(e.target.value, setConfirmPassword)}
+          type="email"
+          placeholder="이메일 입력"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            handleInputChange(e.target.value, setEmail)
+          }
         />
-        <InputRightElement width="4.5rem">
-          <Button h="1.75rem" size="sm"
-          onClick={handleClick}
-          >
-            {show ? "감추기" : "보기"}
-          </Button>
-        </InputRightElement>
-      </InputGroup>
-    </FormControl>
-    <FormControl id="pic">
-      <FormLabel>프로필 사진</FormLabel>
-      <Input
-        type="file"
-        p={1.5}
-        accept="image/*"
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => postDetails(e)}
-      />
-    </FormControl>
-    <Button
-      colorScheme="blue"
-      width="100%"
-      style={{ marginTop: 15 }}
-    // onClick={submitHandler}
-    // isLoading={picLoading}
-    >
-      회원가입
-    </Button>
-  </VStack>
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>비밀번호</FormLabel>
+        <InputGroup size="md">
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="비밀번호 입력"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              handleInputChange(e.target.value, setPassword)
+            }
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "숨기기" : "보기"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id="password-confirm" isRequired>
+        <FormLabel>비밀번호 확인</FormLabel>
+        <InputGroup size="md">
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="비밀번호 확인"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              handleInputChange(e.target.value, setConfirmPassword)
+            }
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "숨기기" : "보기"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id="pic">
+        <FormLabel>프로필 사진</FormLabel>
+        <Input
+          type="file"
+          p={1.5}
+          accept="image/*"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => postDetails(e)}
+        />
+      </FormControl>
+      <Button
+        colorScheme="blue"
+        width="100%"
+        style={{ marginTop: 15 }}
+        // onClick={submitHandler}
+        // isLoading={picLoading}
+      >
+        회원가입
+      </Button>
+    </VStack>
+  );
 }
