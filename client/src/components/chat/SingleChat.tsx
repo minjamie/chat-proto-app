60c37069bdda66651987be111ed6d472ef981273
김민재 <minjae2246@ubcare.co.kr>
2024-04-22 17:14:54 +0900

    front - 채팅 방 메시지 UI 구현 중

--- client/src/components/chat/SingleChat.tsx
+++ client/src/components/chat/SingleChat.tsx
@@ -1,45 +1,66 @@
 import { getSender, getSenderFull } from "@/common/chatLogics";
 import { ChatState } from "@/context/chatProvider";
 import { ArrowBackIcon } from "@chakra-ui/icons";
-import { Box, IconButton, Text } from "@chakra-ui/react";
+import {
+  Box,
+  FormControl,
+  IconButton,
+  Input,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import ProfileModal from "@components/modal/ProfileModal";
 import UpdateGroupChatModal from "@components/modal/UpdateGroupChatModal";
+import { useState } from "react";
 
-export default function SingleChat({fetchAgain, setFetchAgain}) {
-    const { selectedChat, setSelectedChat, user, notification, setNotification } =
-      ChatState();
-  
+export default function SingleChat({ fetchAgain, setFetchAgain }) {
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [isTyping, setIstyping] = useState(false);
+  const [newMessage, setNewMessage] = useState("");
+  const { selectedChat, setSelectedChat, user, notification, setNotification } =
+    ChatState();
+
+  const sendMessage = () => {};
+  const typingHandler = () => {};
+
   return (
-    <>{selectedChat ?
-      <Text
-        fontSize={{ base: "28px", md: "30px" }}
-        pb={3}
-        px={2}
-        w="100%"
-        fontFamily="Work sans"
-        display="flex"
-        justifyContent={{ base: "space-between" }}
-        alignItems="center"
-      >
-        <IconButton
-          display="flex"
-          icon={<ArrowBackIcon />}
-          onClick={() => setSelectedChat("")} aria-label={""} />
-        {!selectedChat.isGroupChat
-            ? 
-          <>
-            {getSender(user, selectedChat.users)}
-            <ProfileModal user={getSenderFull(user, selectedChat.users)} />
-          </>
-          :
-            (
-            <>
-              {selectedChat.chatName.toUpperCase()}
-              <UpdateGroupChatModal fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
-            </>
-          )}
-        {/* <Box
-            d="flex"
+    <>
+      {selectedChat ? (
+        <>
+          <Text
+            fontSize={{ base: "28px", md: "30px" }}
+            pb={3}
+            px={2}
+            w="100%"
+            fontFamily="Work sans"
+            display="flex"
+            justifyContent={{ base: "space-between" }}
+            alignItems="center"
+          >
+            <IconButton
+              display="flex"
+              icon={<ArrowBackIcon />}
+              onClick={() => setSelectedChat("")}
+              aria-label={""}
+            />
+            {!selectedChat.isGroupChat ? (
+              <>
+                {getSender(user, selectedChat.users)}
+                <ProfileModal user={getSenderFull(user, selectedChat.users)} />
+              </>
+            ) : (
+              <>
+                {selectedChat.chatName.toUpperCase()}
+                <UpdateGroupChatModal
+                  fetchAgain={fetchAgain}
+                  setFetchAgain={setFetchAgain}
+                />
+              </>
+            )}
+          </Text>
+          <Box
+            display="flex"
             flexDir="column"
             justifyContent="flex-end"
             p={3}
@@ -48,16 +69,53 @@ export default function SingleChat({fetchAgain, setFetchAgain}) {
             h="100%"
             borderRadius="lg"
             overflowY="hidden"
+          >
+            {loading ? (
+              <Spinner
+                size="xl"
+                w={20}
+                h={20}
+                alignSelf="center"
+                margin="auto"
+              />
+            ) : (
+              <div>메세지</div>
+            )}
 
+            <FormControl onKeyDown={sendMessage} isRequired mt={3}>
+              {isTyping ? (
+                <div>
+                  {/* <Lottie
+                    options={defaultOptions}
+                    width={70}
+                    style={{ marginBottom: 15, marginLeft: 0 }}
+                  /> */}
+                </div>
+              ) : (
+                <></>
+              )}
+              <Input
+                variant="filled"
+                bg="#E0E0E0"
+                placeholder="메시지 입력"
+                value={newMessage}
+                onChange={typingHandler}
+              />
+            </FormControl>
+          </Box>
+        </>
+      ) : (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          h="100%"
         >
-        </Box> */}
-      </Text>
-      :
-      <Box display="flex" alignItems="center" justifyContent="center" h="100%">
-        <Text fontSize="3xl" pb={3} fontFamily="Work sans">
-          유저를 클릭해서 채팅을 시작
-        </Text>
-      </Box>
-}</>
-  )
+          <Text fontSize="3xl" pb={3} fontFamily="Work sans">
+            유저를 클릭해서 채팅을 시작
+          </Text>
+        </Box>
+      )}
+    </>
+  );
 }
