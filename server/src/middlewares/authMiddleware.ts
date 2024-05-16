221807433835218f611dc0ce6f0cccd8eb9b3c13
minjamie <minjae2246@gamil.com>
2024-05-16 22:03:25 +0900

    token  수정

--- server/src/middlewares/authMiddleware.ts
+++ server/src/middlewares/authMiddleware.ts
@@ -15,11 +15,12 @@ const protect = asyncHandler(
     ) {
       try {
         token = req.headers.authorization.split(" ")[1];
-        const decoded = jwt.verify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzNCIsImV4cCI6MTcxNTg2OTA1NywiZSI6InBqY3NvbjhAZGF1bS5uZXQiLCJ2Ijoia2FrYW8iLCJhIjpbIlVTRVIiXX0.8EAz15yCJCCGsOR3MFOa6X_hNXkx4OYjEa_v2DdyeG0", process.env.JWT_SECRET as string) as {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
           sub: any; id?: string 
 };
         const pk = Number(decoded.sub);
         req.user = await User.findById(toObjectHexString(pk)).select("-password");
+        console.log(req.user)
         next();
       } catch (error) {
         res.status(401);
