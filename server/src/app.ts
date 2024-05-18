025ec6ecaee2d37accc5ece5b8f80cfb3dff216c
minjamie <minjae2246@gamil.com>
2024-05-18 14:11:40 +0900

    채팅 소켓 오류 해결

--- server/src/app.ts
+++ server/src/app.ts
@@ -86,7 +86,9 @@ io.on("connection", (socket) => {
     if (!chat.users) return console.log("chat user not defined");
     chat.users.forEach((user: IUserDocument) => {
       if (user._id == newMessageReceived.sender._id) return;
-      socket.in(user._id).emit("message received", newMessageReceived);
+      else {
+        socket.in(chat.id).emit("message received", newMessageReceived);
+      }
     });
   });
 });
