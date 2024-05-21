0483bab847b977722ebdaf302d205802f4232464
Koak DoHun <dori20051@gmail.com>
2024-05-21 16:26:17 +0900

    feat: 유저 정보 수정 & 삭제

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,10 +1,12 @@
-import { userController } from "@controllers/index";
-import { protect } from "@middlewares/authMiddleware";
 import express from "express";
+import { protect } from "@middlewares/authMiddleware";
+import { userController } from "@controllers/index";
 
 const router = express.Router();
 router.get("/", protect, userController.getUsers);
 router.post("/", userController.createUser);
+router.delete("/:id", userController.deleteUser);
+router.patch("/:id", userController.updateUser);
 router.get("/save-key", userController.getUserKey);
 router.post("/sign-up", userController.signUpUser);
 router.post("/sign-in", userController.signInUser);
